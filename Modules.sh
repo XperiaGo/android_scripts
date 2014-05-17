756e60e049563aee1b19a748747907166458ffd7
Info4Geek <admin@info4geek.com>
2014-05-17 20:05:15 +0000

    Modules.sh for all XSTE devices

--- Modules.sh
+++ Modules.sh
@@ -1,14 +1,71 @@
-#!/bin/bash
+#Make modules
+if [[ $# = 1 ]]; then
+  cd kernel/sony/u8500
+  if [[ $? = 0 ]]; then
+    case $1 in
+    -u)
+      export ARCH=arm
+      export CROSS_COMPILE=/home/devhub/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+      make mrproper
+      make xperiau_defconfig
+      make modules
+      cd ../../..
+      mkdir -p out/target/product/kumquat/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*.ko out/target/product/kumquat/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
+    ;;
+    -p)
+      export ARCH=arm
+      export CROSS_COMPILE=/home/devhub/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+      make mrproper
+      make xperiap_defconfig
+      make modules
+      cd ../../..
+      mkdir -p out/target/product/nypon/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*.ko out/target/product/nypon/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/nypon/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/nypon/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/nypon/system/lib/modules/
+    ;;
+    -s)
+      export ARCH=arm
+      export CROSS_COMPILE=/home/devhub/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+      make mrproper
+      make xperiasola_defconfig
+      make modules
+      cd ../../..
+      mkdir -p out/target/product/pepper/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*.ko out/target/product/pepper/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/pepper/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/pepper/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/pepper/system/lib/modules/
+    ;;
+    -g)
+      export ARCH=arm
+      export CROSS_COMPILE=/home/devhub/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+      make mrproper
+      make xperiago_defconfig
+      make modules
+      cd ../../..
+      mkdir -p out/target/product/lotus/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*.ko out/target/product/lotus/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/lotus/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/lotus/system/lib/modules/
+      cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/lotus/system/lib/modules/
+    ;;
+    *)
+      echo "ERROR: Unknow option";
+      exit -1;
+    ;;
+    esac
+  else 
+    echo "ERROR: . build/envsetup.sh falied"
+    exit -1;
+  fi
+else
+  echo "ERROR: Number of options not correct. Usage: ./Modules.sh -u | -p | -s | -g"
+  exit -1;
+fi
 
-cd kernel/sony/u8500
-export ARCH=arm
-export CROSS_COMPILE=/home/devhub/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
-make mrproper
-make xperiau_defconfig
-make modules
-cd ../../..
-mkdir -p out/target/product/kumquat/system/lib/modules/
-cp -fr kernel/sony/u8500/*/*/*.ko out/target/product/kumquat/system/lib/modules/
-cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
-cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
-cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
