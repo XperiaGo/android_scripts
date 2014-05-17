38c099ef3a84b895b7c58af44e0105e768f763b1
Alberto García <garcia98ftw@gmail.com>
2014-05-17 18:34:38 +0000

    Updated Modules.sh

--- Modules.sh
+++ Modules.sh
@@ -4,7 +4,7 @@ cd kernel/sony/u8500
 export ARCH=arm
 export CROSS_COMPILE=/home/devhub/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
 make mrproper
-make kumquat_defconfig
+make xperiau_defconfig
 make modules
 cd ../../..
 mkdir -p out/target/product/kumquat/system/lib/modules/
