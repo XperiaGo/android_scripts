e5e8329169b020511b09d9b2a9fb88c300acb86e
Info4Geek <admin@info4geek.com>
2014-05-18 00:23:24 +0000

    Modules.sh: Build fix

--- Modules.sh
+++ Modules.sh
@@ -2,7 +2,7 @@
 if [[ $# = 1 ]]; then
   cd kernel/sony/u8500
   export ARCH=arm
-  export CROSS_COMPILE=prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+  export CROSS_COMPILE=/home/devhub/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
   if [[ $? = 0 ]]; then
     case $1 in
     -u)
@@ -15,6 +15,8 @@ if [[ $# = 1 ]]; then
       cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
       cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
       cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/kumquat/system/lib/modules/
+      cd kernel/sony/u8500
+      make mrproper
     ;;
     -p)
       make mrproper
@@ -26,6 +28,8 @@ if [[ $# = 1 ]]; then
       cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/nypon/system/lib/modules/
       cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/nypon/system/lib/modules/
       cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/nypon/system/lib/modules/
+      cd kernel/sony/u8500
+      make mrproper
     ;;
     -s)
       make mrproper
@@ -37,6 +41,8 @@ if [[ $# = 1 ]]; then
       cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/pepper/system/lib/modules/
       cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/pepper/system/lib/modules/
       cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/pepper/system/lib/modules/
+      cd kernel/sony/u8500
+      make mrproper
     ;;
     -g)
       make mrproper
@@ -48,15 +54,14 @@ if [[ $# = 1 ]]; then
       cp -fr kernel/sony/u8500/*/*/*/*.ko out/target/product/lotus/system/lib/modules/
       cp -fr kernel/sony/u8500/*/*/*/*/*.ko out/target/product/lotus/system/lib/modules/
       cp -fr kernel/sony/u8500/*/*/*/*/*/*.ko out/target/product/lotus/system/lib/modules/
+      cd kernel/sony/u8500
+      make mrproper
     ;;
     *)
       echo "ERROR: Unknow option";
       exit -1;
     ;;
     esac
-  else 
-    echo "ERROR: . build/envsetup.sh falied"
-    exit -1;
   fi
 else
   echo "ERROR: Number of options not correct. Usage: ./Modules.sh -u | -p | -s | -g"
