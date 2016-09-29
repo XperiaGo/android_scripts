bcc1b3ff0827689469f5e05d15ba019db0cb14ab
HardLight <hardlightxda@gmail.com>
2016-09-29 09:40:14 +0000

    Update fetch script

--- Fetch.sh
+++ Fetch.sh
@@ -2,12 +2,14 @@
 
 branch="cm-11.0"
 remote="CyanogenMod"
+twrpbranch="android-6.0"
+twrpremote="omnirom"
+twrpurl="https://github.com/omnirom/android_bootable_recovery.git"
 urlstart="https://github.com/CyanogenMod/android_"
 urlend=".git"
 gitstart="git@github.com:XperiaGo/android_"
 #gitstart="https://github.com/XperiaGo/android_"
 gitend=".git"
-twrpbranch="android-6.0"
 
 path=(
 art
@@ -18,7 +20,6 @@ external/valgrind
 frameworks/av
 frameworks/base
 frameworks/native
-frameworks/opt/telephony
 hardware/libhardware
 hardware/libhardware_legacy
 packages/apps/Settings
@@ -26,7 +27,6 @@ packages/inputmethods/LatinIME
 system/core
 system/media
 system/vold
-external/koush/Superuser
 vendor/cm
 )
 
@@ -38,115 +38,95 @@ repos=$(echo ${#path[@]} )
 
 read -r -p "Do you wish to add remotes? [y/N] " response
 response=${response,,}  
-if [[ $response =~ ^(yes|y)$ ]]
-then
-for ((i = 0; i < $repos; i++)) 
-do
-echo "Adding remote ${remote} to ${path[i]}"
-# Exception
-if [[ ${path[i]} == bootable/recovery ]]; then
-url="https://github.com/omnirom/android_bootable_recovery.git"
-remote="omnirom"
-elif [[ ${path[i]} == external/koush/Superuser ]]; then
-url="https://github.com/CyanogenMod/Superuser.git"
-remote="CyanogenMod"
-else
-# Generate url from $path
-url="${urlstart}$(echo ${path[i]} | sed 's.\/._.g')${urlend}"
-remote="CyanogenMod"
-fi
-git -C ${path[i]} remote remove ${remote}
-git -C ${path[i]} remote add ${remote} ${url}
-done
+if [[ $response =~ ^(yes|y)$ ]]; then
+  for ((i = 0; i < $repos; i++)); do
+    # TWRP exception
+    if [[ ${path[i]} == bootable/recovery ]]; then
+      echo "Adding remote ${twrpremote} to ${path[i]}"
+      git -C ${path[i]} remote remove ${twrpremote}
+      git -C ${path[i]} remote add ${twrpremote} ${twrpurl}
+    else
+      echo "Adding remote ${remote} to ${path[i]}"
+      # Generate url from $path
+      url="${urlstart}$(echo ${path[i]} | sed 's.\/._.g')${urlend}"
+      git -C ${path[i]} remote remove ${remote}
+      git -C ${path[i]} remote add ${remote} ${url}
+    fi
+  done
 echo
 fi
 
 read -r -p "Do you wish to fetch remotes? [y/N] " response
 response=${response,,}
-if [[ $response =~ ^(yes|y)$ ]]
-then
-for ((i = 0; i < $repos; i++)) 
-do
-echo "Fetching ${path[i]}"
-# Exception
-if [[ ${path[i]} == bootable/recovery ]]; then
-remote="omnirom"
-fi
-git -C ${path[i]} fetch ${remote} 
-remote="CyanogenMod"
-done
+if [[ $response =~ ^(yes|y)$ ]]; then
+  for ((i = 0; i < $repos; i++)); do
+    echo "Fetching ${path[i]}"
+    # TWRP exception
+    if [[ ${path[i]} == bootable/recovery ]]; then
+      git -C ${path[i]} fetch ${twrpremote} ${twrpbranch}
+    else
+      git -C ${path[i]} fetch ${remote} ${branch}
+    fi
+  done
 echo
 fi
 
 read -r -p "Do you wish to change branches? [y/N] " response
 response=${response,,}  
-if [[ $response =~ ^(yes|y)$ ]]
-then
-for ((i = 0; i < $repos; i++)) 
-do
-# Exception
-if [[ ${path[i]} == bootable/recovery ]]; then
-branch=$twrpbranch
-else
-branch="cm-11.0"
-fi
-echo "Changing ${path[i]} branch  to $branch"
-git -C ${path[i]} branch temp
-git -C ${path[i]} checkout temp
-git -C ${path[i]} branch -D $branch
-git -C ${path[i]} branch $branch
-git -C ${path[i]} checkout $branch
-git -C ${path[i]} branch -D temp
-done
+if [[ $response =~ ^(yes|y)$ ]]; then
+  for ((i = 0; i < $repos; i++)); do
+# TWRP exception
+    if [[ ${path[i]} == bootable/recovery ]]; then
+      echo "Changing ${path[i]} branch to $twrpbranch"
+      git -C ${path[i]} branch temp
+      git -C ${path[i]} checkout temp
+      git -C ${path[i]} branch -D $twrpbranch
+      git -C ${path[i]} branch $twrpbranch
+      git -C ${path[i]} checkout $twrpbranch
+      git -C ${path[i]} branch -D temp
+    else
+      echo "Changing ${path[i]} branch to $branch"
+      git -C ${path[i]} branch temp
+      git -C ${path[i]} checkout temp
+      git -C ${path[i]} branch -D $branch
+      git -C ${path[i]} branch $branch
+      git -C ${path[i]} checkout $branch
+      git -C ${path[i]} branch -D temp
+    fi
+  done
 echo
 fi
 
 read -r -p "Do you wish to merge changes? [y/N] " response
 response=${response,,}  
-if [[ $response =~ ^(yes|y)$ ]]
-then
-for ((i = 0; i < $repos; i++)) 
-do
-echo "Merging changes to ${path[i]}"
-# Exception
-if [[ ${path[i]} == bootable/recovery ]]; then
-remote="omnirom"
-branch=$twrpbranch
-else
-remote="CyanogenMod"
-branch="cm-11.0"
-fi
-git -C ${path[i]} merge ${remote}/${branch}
-echo
+if [[ $response =~ ^(yes|y)$ ]]; then
+  for ((i = 0; i < $repos; i++)); do
+    echo "Merging changes to ${path[i]}"
+    # TWRP exception
+    if [[ ${path[i]} == bootable/recovery ]]; then
+      git -C ${path[i]} merge ${twrpremote}/${twrpbranch}
+    else
+      git -C ${path[i]} merge ${remote}/${branch}
+    fi
+  echo
 done
 fi
 
 read -r -p "Do you wish to push changes? [y/N] " response
 response=${response,,}  
-if [[ $response =~ ^(yes|y)$ ]]
-then
-
-#eval `ssh-agent -s`
-#ssh-add
-
-for ((i = 0; i < $repos; i++)) 
-do
-# Exception
-if [[ ${path[i]} == bootable/recovery ]]; then
-branch=$twrpbranch
-else
-branch="cm-11.0"
-fi
-echo "Pushing ${path[i]} changes"
-# Exception
-if [[ ${path[i]} == external/koush/Superuser ]]; then
-git="git@github.com:XperiaGo/Superuser.git"
-else
-git="${gitstart}$(echo ${path[i]} | sed 's.\/._.g')${gitend}"
-fi
-git -C ${path[i]} push ${git} ${branch}
-done
+if [[ $response =~ ^(yes|y)$ ]]; then
+  #eval `ssh-agent -s`
+  #ssh-add
+  for ((i = 0; i < $repos; i++)); do
+    echo "Pushing ${path[i]} changes"
+    git="${gitstart}$(echo ${path[i]} | sed 's.\/._.g')${gitend}"
+    # TWRP exception
+    if [[ ${path[i]} == bootable/recovery ]]; then
+      git -C ${path[i]} push ${git} ${twrpbranch}
+    else
+      git -C ${path[i]} push ${git} ${branch}
+    fi
+  done
 fi
 
 exit
-
