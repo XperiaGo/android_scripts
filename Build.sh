f3d5b09a1a03f72de09a20ba02ed63896080b8d3
hardlightxda@gmail.com <hardlightxda@gmail.com>
2016-12-13 11:00:04 +0000

    Build.sh: Add manifest generation

--- Build.sh
+++ Build.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+CM="cm-11.0"
 VERSION="-$(date -u +%Y%m%d)"
 CCASHE=true
 
 # Help
 if [[ "$*" == "-"* ]] && [[ "$*" == *"h"* ]]; then
   echo -e "Usage: $0 [OPTION]\n"
-  echo -e " -p		Get prebuilts\n -c		Clobber\n -b		Bootimage"
+  echo -e " -p		Get prebuilts\n -c		Clobber\n -b		Bootimage\n -s             Skip generating manifest"
   exit
+fi
+
+# Generate manifest
+if [[ "$*" == "-"* ]] && [[ "$*" == *"s"* ]]; then
+: # null command
+else
+	echo -e "\e[32mGenerating manifest $CM$VERSION.xml\e[0m"
+	if ! [ -f .repo/manifests_history/$CM$VERSION.xml ]; then
+
+		repo manifest -r -o .repo/manifests_history/$CM$VERSION.xml
+	else
+                echo -e "\e[31mFile .repo/manifests_history/$CM$VERSION.xml already exists, aborting. \e[0m"
+		exit 1
+	fi
 fi
 
 # Use local Java Development Kit 6
@@ -27,7 +42,7 @@ if [[ "$*" == "-"* ]] && [[ "$*" == *"c"* ]]; then
   make clobber
 fi
 
-#Get prebuilts
+# Get prebuilts
 if [[ "$*" == "-"* ]] && [[ "$*" == *"p"* ]] || [ ! -f vendor/cm/proprietary/Term.apk ]; then
   echo "Getting prebuilts..."
   cd vendor/cm && ./get-prebuilts && cd ../..
@@ -37,7 +52,7 @@ fi
 if [[ "$CCASHE" == "true" ]]; then
   export USE_CCACHE=1
   export CCACHE_DIR=$(realpath ../ccashe)
-  prebuilts/misc/linux-x86/ccache/ccache -M 10G
+  prebuilts/misc/linux-x86/ccache/ccache -M 50G
 fi
 
 # Version
@@ -46,7 +61,7 @@ export PRODUCT_VERSION_DEVICE_SPECIFIC=$VERSION
 export KBUILD_BUILD_USER=HardLight
 export KBUILD_BUILD_HOST=BuildDroid
 
-#Compile
+# Build
 . build/envsetup.sh
 lunch cm_lotus-userdebug
 if [[ "$*" == "-"* ]] && [[ "$*" == *"b"* ]]; then
